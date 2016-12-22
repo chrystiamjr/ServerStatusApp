0c8b3b69b567de20434c7fcebc02d305afa1b386
Chrystiam Junior <chrystiamjr@gmail.com>
2016-12-21 21:30:43 -0300

    First commit of ServerStatusApp - PI 2016
    This project is a ‘PI’ from IESAM made on 2016 which uses a online
    database to gather information from an Arduino and then sync this data
    with a mobile app

---
+++ www/js/app.js
@@ -0,0 +1,126 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('myPiApp', ['ionic', 'myPiApp.controllers', 'myPiApp.services','ngStorage'])
+
+.run(function($ionicLoading, $ionicPlatform, $localStorage, $sessionStorage, $http, $timeout, $state) {
+  $ionicLoading.show({
+    content: '',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 100,
+    showDelay: 100,
+    duration: 3000
+  }).then(function(){
+    console.log("The loading indicator is now displayed");
+  });
+  $ionicPlatform.ready(function() {
+
+    // Checa se o usuário já estava logado ou não!
+    if($localStorage.token.length != 0) $state.go('tab.dash');
+    else $state.go('login');
+
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if (window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+
+    $storage = $localStorage.$default({
+      templog: {},
+      token: [],
+      user: [],
+      usuarios:{}
+    });
+    $storage = $sessionStorage.$default({
+      user: []
+    })
+
+  });
+
+  var retrieve  = function () {
+
+    $http.get("http://pi2016.16mb.com/php/getAllHistory.php", {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+    .success(function (res) {
+      $localStorage.templog = res;
+      // console.log($storage);
+    })
+
+    $http.get("http://pi2016.16mb.com/php/getUserData.php", {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+    .success(function (res) {
+      $localStorage.usuarios = res;
+      console.log($localStorage.usuarios);
+      console.log($localStorage.templog);
+      // alert($localStorage.usuarios);
+    })
+
+    $timeout(retrieve, 60000); //290000
+    // alert($localStorage.templog.length);
+  }
+  // delete $localStorage.usuarios;
+  $timeout(retrieve, 500);
+})
+
+// Mantém as tabs sempre em baixo
+.config(['$ionicConfigProvider', function($ionicConfigProvider) {
+  $ionicConfigProvider.tabs.position('bottom'); // other values: top
+}])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+  .state('login', {
+    url: "/login",
+    templateUrl: "templates/login.html",
+    controller: 'LoginCtrl'
+  })
+
+  .state('tab',{
+    url: '/tab',
+    abstract: true,
+    templateUrl: 'templates/tab.html'
+  })
+
+  .state('tab.dash',{
+    url: '/dash',
+    views: {
+      'dash': {
+        templateUrl: 'templates/dashboard.html',
+        controller: 'DashCtrl'
+      }
+    }
+  })
+
+  .state('tab.live',{
+    url: '/live',
+    views: {
+      'live': {
+        templateUrl: 'templates/liveTemp.html',
+        controller: 'LiveCtrl'
+      }
+    }
+  })
+
+  .state('tab.history',{
+    url: '/history',
+    views: {
+      'history': {
+        templateUrl: 'templates/historyTemp.html',
+        controller: 'HistoryCtrl'
+      }
+    }
+  });
+
+  $urlRouterProvider.otherwise('/login');
+
+})
---
+++ www/js/controllers.js
@@ -0,0 +1,192 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+angular.module('myPiApp.controllers', ['ngStorage'])
+
+.controller('LogoutCtrl', function($state, $localStorage, $sessionStorage, $scope, $ionicHistory){
+  $scope.logout = function(){
+    $localStorage.$reset({ token: [], user: [] });
+    $sessionStorage.$reset({ user: [] });
+    $ionicHistory.clearHistory();
+    $ionicHistory.clearHistory();
+    $state.go('login');
+  }
+})
+
+.controller('LoginCtrl', function ($scope, $ionicModal, $state, $http, $ionicPopup, $localStorage, $sessionStorage) {
+
+  console.log('Login Controller Initialized');
+
+  $ionicModal.fromTemplateUrl('templates/signup.html', {
+    scope: $scope
+  }).then(function (modal) {
+    $scope.modal = modal;
+  });
+
+
+  // $scope.data = [];
+
+  $scope.createUser = function (user) {
+    var link = 'http://pi2016.16mb.com/php/cadastroUsuario.php';
+    // alert(user.password.length);
+    if(user.username.length > 5 && user.password.length > 5){
+      if (user && user.username && user.password) {
+        var dados = {
+          username: user.username,
+          password: user.password
+        }
+        $http.post(link, dados, {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+        .then(function (res) {
+          var teste = res.data;
+          var resultado = teste.length;
+          if (resultado <= 7) {
+            var alertPopup = $ionicPopup.alert({
+              title: 'Sucesso!',
+              template: 'Credenciais cadastradas com sucesso!'
+            });
+            $scope.modal.hide();
+          }
+          $scope.response = res.data;
+
+        });
+      }
+    }else{
+      var alertPopup = $ionicPopup.alert({
+        title: 'Erro: Tamanho insuficente!',
+        template: 'Username e Senha não podem ser menores que 5 caracteres!'
+      });
+    }
+  };
+  var result = null;
+
+  $scope.signIn = function (userLogin) {
+    if (userLogin && userLogin.username && userLogin.passwordLogin) {
+      for(var i = 0; i<$localStorage.usuarios.length;i++) {
+        if ($localStorage.usuarios[i].username == userLogin.username && $localStorage.usuarios[i].senha == userLogin.passwordLogin){
+          console.log("OK");
+          result = "sucesso";
+
+          if(userLogin.reminder){
+            // Atribui um token codificado em base64 associado ao usuário
+            // alert(btoa(userLogin.username));
+            $localStorage.token.push(btoa(userLogin.username));
+            $localStorage.user.push(userLogin.username);
+            // $localStorage.token.user.push(userLogin.username);
+          }else{
+            $sessionStorage.user.push(userLogin.username);
+          }
+
+          $state.go('tab.dash');
+          break;
+        } else {
+          result = "falhou";
+        }
+      }
+
+      if(result == "sucesso"){
+
+        userLogin.username = null;
+        userLogin.passwordLogin = null;
+        userLogin.reminder = null;
+
+        var alertPopup = $ionicPopup.alert({
+          title: 'Sucesso!',
+          template: 'Usuário Logado!'
+        });
+      } else {
+
+        userLogin.username = null;
+        userLogin.passwordLogin = null;
+        userLogin.reminder = null;
+
+        var alertPopup = $ionicPopup.alert({
+          title: 'Erro ao efetuar login!',
+          template: 'Email ou Senha estão incorretos!'
+        });
+      }
+      // var link = 'http://pi2016.16mb.com/php/loginUsuario.php';
+      // var dados = {
+      //   username: userLogin.username,
+      //   password: userLogin.passwordLogin
+      // }
+      // $http.post(link, dados, {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+      // .then(function (res) {
+      //   var teste2 = res.data;
+      //   var resultado2 = teste2.length;
+      //   if (resultado2 <= 7) {
+    }
+  };
+})
+
+.controller('DashCtrl', function($scope, $http, $localStorage, $sessionStorage){
+  $scope.$watch(function(){
+    if($localStorage.user[0]){
+      $scope.usuario = $localStorage.user[0];
+    }else{
+      $scope.usuario = $sessionStorage.user[0];
+    }
+  })
+})
+
+.controller('LiveCtrl', function ($scope, $timeout, $http, $ionicLoading) {
+
+  // console.log($localStorageProvider.get('history'));
+  $scope.live = [];
+  $ionicLoading.show({
+    content: 'Carregando Dados',
+    animation: 'fade-in',
+    showBackdrop: true,
+    maxWidth: 100,
+    duration: 2000
+  }).then(function(){
+    console.log("The loading indicator is now displayed");
+  });
+  var getLive = function () {
+    $http.get("http://pi2016.16mb.com/php/getLiveTemp.php", {headers: {'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'}})
+    .success(function (res) {
+      $scope.live = res;
+    })
+
+    // if($localStorage.templog){
+    //   $scope.live = $localStorage.templog; // Funciona no safari
+    // }else {
+    //   $scope.live = $localStorage.templog.data; // Funciona no mozilla
+    // }
+
+    $timeout(getLive, 500); // Timeout de quase 5 min
+  }
+  $timeout(getLive, 500); // Timeout de quase 5 min
+
+})
+
+.controller('HistoryCtrl', function ($scope, $ionicModal, $localStorage) {
+
+  $scope.resultados = [];
+
+  $ionicModal.fromTemplateUrl('templates/AllHistory.html', {
+    scope: $scope
+  }).then(function (geral) {
+    $scope.geral = geral;
+  });
+
+  if($localStorage.templog){
+    $scope.resultados = $localStorage.templog; // Funciona no safari
+  }else {
+    $scope.resultados = $localStorage.templog.data; // Funciona no mozilla
+  }
+
+})
+
+.controller('AllHistoryCtrl', function ($scope, $localStorage) {
+
+  $scope.resultadosGerais = [];
+
+  if($localStorage.templog){
+    $scope.resultadosGerais = $localStorage.templog; // Funciona no safari
+  }else {
+    $scope.resultadosGerais = $localStorage.templog.data; // Funciona no mozilla
+  }
+
+});
